9b82858a6c0e2a266fa6d5d97e1aa70d2d532cd8
JColamaio <jcolamaio@gmail.com>
2022-06-14 19:28:33 -0300

    final commit

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-let scoreHome = document.getElementById("scoreNum");
-console.log(scoreHome)
-let count = 0;
+let scoreHomeA = document.getElementById("scoreNumA");
+let scoreHomeB = document.getElementById("scoreNumB");
+let countA = 0;
+let countB = 0;
 
 function increment1() {
-    count += 1
-    scoreHome.textContent = count
+    countA += 1
+    scoreHomeA.textContent = countA
 }
 function increment2() {
-    count += 2
-    scoreHome.textContent = count
+    countA += 2
+    scoreHomeA.textContent = countA
 }
 function increment3() {
-    count += 3
-    scoreHome.textContent = count
+    countA += 3
+    scoreHomeA.textContent = countA
+}
+function increment1G() {
+    countB += 1
+    scoreHomeB.textContent = countB
+}
+function increment2G() {
+    countB += 2
+    scoreHomeB.textContent = countB
+}
+function increment3G() {
+    countB += 3
+    scoreHomeB.textContent = countB
+}
+
+function reset(){
+    countA = 0
+    scoreHomeA.textContent = 0;
+    countB = 0
+    scoreHomeB.textContent = 0;
+
 }
